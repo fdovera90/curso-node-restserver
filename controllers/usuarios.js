ab99ed757f7adb5061361ae6d07680845ca807a6
Fernando Vera <fernandoveragonzalez@gmail.com>
2021-10-29 11:49:43 -0300

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,47 @@
+const { request, response } = require('express');
+
+const usuariosGet = (req = request, res = response ) => {
+
+    const {q, nombre, apiKey} = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apiKey
+    });
+}
+
+const usuariosPost = (req, res = response ) => {
+    
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response ) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosDelete = (req, res = response ) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
